bbbe2130db457f2526629dd31405107cfddb9bbd
Scott Miller <scott@coder.com>
2026-08-05 17:56:34 +0000

    docs(coderd/pubsub): simplify ProvisionerKeyDeletedChannel doc comment

--- coderd/pubsub/provisionerkeydeleted.go
+++ coderd/pubsub/provisionerkeydeleted.go
@@ -3,8 +3,7 @@ package pubsub
 import "github.com/google/uuid"
 
 // ProvisionerKeyDeletedChannel returns the pubsub channel that carries a
-// notification when the provisioner key with the given ID is deleted. The
-// payload is empty; the channel name encodes the key ID.
+// notification when the provisioner key with the given ID is deleted.
 func ProvisionerKeyDeletedChannel(keyID uuid.UUID) string {
 	return "provisioner_key_deleted:" + keyID.String()
 }
